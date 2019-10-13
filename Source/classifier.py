c09231e2b044ee810516d6a2d6890297b31050a1
Neeraj Kumar <nrj1024@gmail.com>
2019-10-13 22:49:21 +0530

    Update classifier.py

--- Source/classifier.py
+++ Source/classifier.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential
-from keras.optimizers import Nadam
-from keras.layers import Conv2D, MaxPooling2D, BatchNormalization
-from keras.layers import Activation, Dropout, Flatten, Dense
-from keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Nadam
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, BatchNormalization
+from tensorflow.keras.layers import Activation, Dropout, Flatten, Dense
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 from livelossplot import PlotLossesKeras
 from PIL import Image
 import numpy as np
